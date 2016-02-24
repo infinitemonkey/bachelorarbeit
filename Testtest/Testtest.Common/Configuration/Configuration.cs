72ddb6c0a687ce9d31c9cd73aee430a63665cc79
Stefan Sidler <stefan.sidler@swisscom.com>
2016-02-24 22:40:02 +0100

    test

--- Testtest/Testtest.Common/Configuration/Configuration.cs
+++ Testtest/Testtest.Common/Configuration/Configuration.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using PCLStorage;
@@ -16,8 +18,16 @@ namespace Testtest.Common
 
         public static async Task<Configuration> Load()
         {
-            IFile file = await FileSystem.Current.LocalStorage.GetFileAsync("test.json");
-            return JsonConvert.DeserializeObject<Configuration>(await file.ReadAllTextAsync());
+            var assembly = typeof(Configuration).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("Testtest.Common.test.json");
+            string text = "";
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+            //IFile file = await FileSystem.Current.LocalStorage.GetFileAsync("test.json");
+            //return JsonConvert.DeserializeObject<Configuration>(await file.ReadAllTextAsync());
+            return JsonConvert.DeserializeObject<Configuration>(text);
         }
     }
 }
